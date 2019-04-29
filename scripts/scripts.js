137c7573d9f7e494806375cb82d386e93cdd3260
Eduardo Grigoletto <eduardo.grigoletto@gmail.com>
2019-04-29 14:40:56 -0300

    Adicionada função para testes com branch. Criada a branch feature1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,11 @@
 function testeAlerta(){
-    alert("Eu sou um teste mas para git")
+    alert("Eu sou um teste mas para git");
 }
 
 function somaNum(num1,num2){
-    return num1+num2
+    return num1+num2;
+}
+
+function multiNum(num1,num2){
+    return num1*num2;
 }
